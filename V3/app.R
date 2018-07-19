4eb76b24c8446028b95b1221cb9973816960ca4c
zhikangx <kyle.zhikang@gmail.com>
2018-07-19 16:08:14 +0800

    some update

--- V3/app.R
+++ V3/app.R
@@ -1,21 +1,29 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-library(leaflet)
-
 if (1==1) {
-        library(leaflet)
-        library(scales)
-        library(lattice)
-        library(DT)
-        library(shiny)
+        
+        if(!require("leaflet")){
+                install.packages("leaflet")
+                library("leaflet")
+        }
+        
+        if(!require("scales")){
+                install.packages("scales")
+                library("scales")
+        }
+        
+        if(!require("lattice")){
+                install.packages("lattice")
+                library("lattice")
+        }
+        
+        if(!require("DT")){
+                install.packages("DT")
+                library("DT")
+        }
+        
+        if(!require("shiny")){
+                install.packages("shiny")
+                library("shiny")
+        }
         
         if(!require("geosphere")){
                 install.packages("geosphere")
